c31f9780ab9ea7d09ff24083f92fa0ce1208aefa
Hezekiah Michael <pianycist@gmail.com>
2023-09-19 14:10:07 -0400

    versioning script works now

--- build/version_bump.sh
+++ build/version_bump.sh
@@ -4,17 +4,23 @@ if [[ $1 ]]; then
     VERSION=$1
     for f in firefox chrome
     do
-        echo $f" "$VERSION
-        jq --arg VERSION $VERSION -r '.version = $VERSION' manifest.json.$f | tee manifest.json.$f
+        OLDVER=$(jq -r '.version' manifest.json.$f)
+        echo $f" $OLDVER => $VERSION"
+        cp manifest.json.$f manifest_old.json.$f
+        jq --arg VERSION "$VERSION" -r '.version = $VERSION' manifest.json.$f > manifest_new.json.$f
+        mv manifest_new.json.$f manifest.json.$f
     done
 else
     for f in firefox chrome
     do
         MAJVER=$(jq -r '.version | match("\\d+\\.\\d+") | .string' manifest.json.$f)
         MINVER=$(jq -r '.version | match("\\d+$") | .string' manifest.json.$f)
-        NEWMINVER=$(calc -p -d $MINVER + 1)
+        NEWMINVER=$(calc -p -d "$MINVER + 1")
+        OLDVER="$MAJVER.$MINVER"
         NEWVER="$MAJVER.$NEWMINVER"
-        echo $f" "$NEWVER
-        jq --arg VERSION $NEWVER -r '.version = $VERSION' manifest.json.$f | tee manifest.json.$f
+        echo $f" $OLDVER => $NEWVER"
+        cp manifest.json.$f manifest_old.json.$f
+        jq --arg VERSION "$NEWVER" -r '.version = $VERSION' manifest.json.$f > manifest_new.json.$f
+        mv manifest_new.json.$f manifest.json.$f
     done
 fi
