35c0d5fb615bd50a5707845a02a53df8eed04bb1
Hezekiah Michael <spiritomb@protonmail.com>
2023-09-19 13:01:05 -0400

    manual versioning option

--- build/version_bump.sh
+++ build/version_bump.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-for f in firefox chrome
-do
-    MAJVER=$(jq -r '.version | match("\\d+\\.\\d+") | .string' manifest.json.$f)
-    MINVER=$(jq -r '.version | match("\\d+$") | .string' manifest.json.$f)
-    NEWMINVER=$(calc -p -d $MINVER + 1)
-    NEWVER="$MAJVER.$NEWMINVER"
-    echo $f" "$NEWVER
-    jq --arg VERSION $NEWVER -r '.version = $VERSION' manifest.json.$f | tee manifest.json.$f
-done
+if [[ $1 ]]; then
+    VERSION=$1
+    for f in firefox chrome
+    do
+        echo $f" "$VERSION
+        jq --arg VERSION $VERSION -r '.version = $VERSION' manifest.json.$f | tee manifest.json.$f
+    done
+else
+    for f in firefox chrome
+    do
+        MAJVER=$(jq -r '.version | match("\\d+\\.\\d+") | .string' manifest.json.$f)
+        MINVER=$(jq -r '.version | match("\\d+$") | .string' manifest.json.$f)
+        NEWMINVER=$(calc -p -d $MINVER + 1)
+        NEWVER="$MAJVER.$NEWMINVER"
+        echo $f" "$NEWVER
+        jq --arg VERSION $NEWVER -r '.version = $VERSION' manifest.json.$f | tee manifest.json.$f
+    done
+fi
