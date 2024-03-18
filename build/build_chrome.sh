cff05aa509ffae0f073a3f9a781c90af6e403d1e
Hezekiah Michael <spiritomb@protonmail.com>
2024-03-17 21:49:49 -0400

    use npm and updated jquery

--- build/build_chrome.sh
+++ build/build_chrome.sh
@@ -5,10 +5,13 @@ pushd .. > /dev/null
 rm -rf dist
 rm -f availablereads-chrome.zip
 
+cp node_modules/jquery/dist/jquery.min.js src/
+cp node_modules/jquery/dist/jquery.min.js src/options/
+
 mkdir dist
 cp -r src dist/
 cp -r icons dist/
-VERSION=$(jq -r '.version' build/manifest.json.firefox)
+VERSION=$(jq -r '.version' build/manifest.json.chrome)
 cp build/manifest.json.chrome dist/manifest.json
 
 pushd dist > /dev/null
